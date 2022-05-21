c0b64f6d9ebebf792c8e036ec3ca890483dbf150
Alex <87553279+Amourousity@users.noreply.github.com>
2022-05-21 10:04:35 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,39 +1,15 @@
-do
-	local Missing = {}
-	for _,Names in pairs{
-		"isfile",
-		"getgenv",
-		"readfile",
-		"writefile",
-		"getcustomasset/getsynasset"
+if isfile then
+	for _,FileName in pairs{
+		"Logo.png",
+		"Source.lua",
+		"Version.ver"
 	} do
-		Names = Names:split("/")
-		local Exists
-		for _,FunctionName in pairs(Names) do
-			if (getgenv and getgenv() or getfenv())[FunctionName] then
-				(getgenv and getgenv() or getfenv())[Names[1]] = (getgenv and getgenv() or getfenv())[FunctionName]
-				Exists = true
-				break
-			end
+		local Success,Result = pcall(game.HttpGet,game,("https://raw.githubusercontent.com/Amourousity/Ultimatum/main/%s"):format(FileName),true)
+		if Success and (not isfile(("Ultimatum%s"):format(FileName)) or Result:gsub("%s","") ~= readfile(("Ultimatum%s"):format(FileName)):gsub("%s","")) then
+			writefile(("Ultimatum%s"):format(FileName),Result)
+		elseif not Success and not isfile(("Ultimatum%s"):format(FileName)) then
+			error(("\nUltimatum | %s"):format(Result))
 		end
-		if not Exists then
-			table.insert(Missing,Names[1])
-		end
-	end
-	assert(#Missing < 1,("Ultimatum | Missing function%s '%s'"):format(1 < #Missing and "s" or "",table.concat(Missing,"', '")))
-	end
-for _,FileName in pairs{
-	"Logo.png",
-	"Source.lua",
-	"Version.ver"
-} do
-	local Success,Result = pcall(game.HttpGet,game,("https://raw.githubusercontent.com/Amourousity/Ultimatum/main/%s"):format(FileName),true)
-	if Success and (not isfile(("Ultimatum%s"):format(FileName)) or Result:gsub("%s","") ~= readfile(("Ultimatum%s"):format(FileName)):gsub("%s","")) then
-		writefile(("Ultimatum%s"):format(FileName),Result)
-	elseif not Success and not isfile(("Ultimatum%s"):format(FileName)) then
-		(consoleprint or rconsoleprint or string.len)("@@RED@@");
-		(consoleprint or rconsoleprint or error)(("\nUltimatum | %s"):format(Result))
-		return
 	end
 end
 loadstring(readfile("UltimatumSource.lua"),"Ultimatum")()
