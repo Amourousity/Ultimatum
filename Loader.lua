35898320c7f5151d2f1522c0e4c48052bdde5fbc
Alex <87553279+Amourousity@users.noreply.github.com>
2022-05-21 10:09:01 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,8 +8,12 @@ if isfile then
 		if Success and (not isfile(("Ultimatum%s"):format(FileName)) or Result:gsub("%s","") ~= readfile(("Ultimatum%s"):format(FileName)):gsub("%s","")) then
 			writefile(("Ultimatum%s"):format(FileName),Result)
 		elseif not Success and not isfile(("Ultimatum%s"):format(FileName)) then
-			error(("\nUltimatum | %s"):format(Result))
+			error(("\nUltimatum | %s"):format(tostring(Result)))
 		end
 	end
+	loadstring(readfile("UltimatumSource.lua"),"Ultimatum")()
+else
+	local Success,Result = pcall(game.HttpGet,game,"https://raw.githubusercontent.com/Amourousity/Ultimatum/main/Source.lua",true)
+	assert(Success,("\nUltimatum | %s"):format(tostring(Result)))
+	loadstring(Result,"Ultimatum")()
 end
-loadstring(readfile("UltimatumSource.lua"),"Ultimatum")()
