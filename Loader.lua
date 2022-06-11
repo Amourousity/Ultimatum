50fdc4ca8f6e1586aa11acf03c1bc5eef4ba8e27
Lex <87553279+Amourousity@users.noreply.github.com>
2022-06-10 20:18:51 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,15 +5,16 @@
  [|]    [|] [|]        [|]         [|]     [|]       [|] [|]     [|] [|]     [|]    [|] [|]       [|]
 [|]    [|] [|]        [|]         [|]     [|]       [|] [|]     [|] [|]     [|]    [|] [|]       [|]
 [||||||]  [||||||||] [|]     [|||||||||] [|]       [|] [|]     [|] [|]      [||||||]  [|]       []]
-local Success,Result = pcall(game.HttpGet,game,"https://raw.githubusercontent.com/Amourousity/Ultimatum/main/Source.lua",true)
-if isfile then
-	if Success and (not isfile"Source.Ultimatum" or Result ~= readfile"Source.Ultimatum") then
-		writefile("Source.Ultimatum",Result)
-	elseif not Success and not isfile"Source.Ultimatum" then
-		error("Ultimatum | "..tostring(Result),0)
-	end
+if isfile and isfile"Source.Ultimatum" then
 	loadstring(readfile"Source.Ultimatum","Ultimatum")()
-else
-	assert(Success,("Ultimatum | %s"):format(tostring(Result)))
-	loadstring(Result,"Ultimatum")()
+	return
+end
+local Success,Result = pcall(game.HttpGet,game,"https://raw.githubusercontent.com/Amourousity/Ultimatum/main/Source.lua",true)
+if Success then
+	if isfile then
+		writefile("Source.Ultimatum",Result)
+		loadstring(readfile"Source.Ultimatum","Ultimatum")()
+	else
+		loadstring(Result,"Ultimatum")()
+	end
 end
