22ad001083ccdd6fac8f6585fa249dc7a5b3fb36
Lex <87553279+Amourousity@users.noreply.github.com>
2022-07-05 22:04:11 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,16 +5,23 @@
  [|]    [|] [|]        [|]         [|]     [|]       [|] [|]     [|] [|]     [|]    [|] [|]       [|]
 [|]    [|] [|]        [|]         [|]     [|]       [|] [|]     [|] [|]     [|]    [|] [|]       [|]
 [||||||]  [||||||||] [|]     [|||||||||] [|]       [|] [|]     [|] [|]      [||||||]  [|]       []]
-if isfile and isfile"Source.Ultimatum" then
-	loadstring(readfile"Source.Ultimatum","Ultimatum")()
-	return
+local UltimatumStart,Ultimatum = os.clock()
+local function Set(Source)
+	Ultimatum = loadstring(Source or readfile"Source.Ultimatum","Ultimatum")
 end
-local Success,Result = pcall(game.HttpGet,game,"https://raw.githubusercontent.com/Amourousity/Ultimatum/main/Source.lua",true)
-if Success then
-	if isfile then
-		writefile("Source.Ultimatum",Result)
-		loadstring(readfile"Source.Ultimatum","Ultimatum")()
-	else
-		loadstring(Result,"Ultimatum")()
+if isfile and isfile"Source.Ultimatum" then
+	Set()
+else
+	local Success,Result = pcall(game.HttpGet,game,"https://raw.githubusercontent.com/Amourousity/Ultimatum/main/Source.lua",true)
+	if Success then
+		if isfile then
+			writefile("Source.Ultimatum",Result)
+			Set()
+		else
+			Set(Result)
+		end
 	end
 end
+local Environment = getfenv(Ultimatum)
+Environment.UltimatumStart = UltimatumStart
+setfenv(Ultimatum,Environment)()
