a5cc0d811a97b4175b6f7b9ca678d95159a884e7
Lex <87553279+Amourousity@users.noreply.github.com>
2022-11-08 02:14:49 -0500

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -807,8 +807,10 @@ for Replace,Info in ({
 									Gun = Color3.new(.5,0,1),
 									Hero = Color3.new(1,1,0),
 									Sheriff = Color3.new(0,0,1),
+									Zombie = Color3.new(0,.5,0),
 									Innocent = Color3.new(0,1,0),
 									Murderer = Color3.new(1,0,0),
+									Survivor = Color3.new(0,0,1)
 								})[Role]
 							}
 						},
@@ -832,6 +834,109 @@ for Replace,Info in ({
 					})
 				end
 			}
+		},
+		AutoFarm_autoplay_autop_autof_farm_af = {
+			Function = function(Variables,Enabled)
+				if not Assert(not (Variables.Enabled and Enabled),"Auto-farm is already enabled",not (not Variables.Enabled and not Enabled),"Auto-farm is already disabled") then
+					return
+				end
+				if Enabled then
+					Variables.Delta,Variables.LastFrame,Variables.Coins,Variables.IgnoreCoins,Variables.Position = 0,os.clock(),{},{},workspace.CurrentCamera.Focus.Position
+					Variables.CoinAdded = Connect(workspace.DescendantAdded,function(Coin)
+						if Coin:IsA"BasePart" and Coin.Name == "Coin_Server" then
+							table.insert(Variables.Coins,Coin)
+							local Connection
+							Connection = Connect(Coin.AncestryChanged,function()
+								if not Coin:IsDescendantOf(workspace) then
+									table.remove(Variables.Coins,table.find(Variables.Coins,Coin))
+									RemoveConnections{
+										Connection
+									}
+								end
+							end)
+							AddConnections{
+								Connection
+							}
+						end
+					end)
+					Variables.CharacterAdded = Connect(Owner.CharacterAdded,function(Character)
+						Variables.Collecting = true
+						Variables.Position = Character:GetPivot().Position
+						Wait(5)
+						Variables.Collecting = false
+						RunCommand"Invisible"
+						Wait(WaitForSequence(Owner,"PlayerGui","MainGUI","Game","CashBag","Full"):GetPropertyChangedSignal"Visible")
+						Character:BreakJoints()
+					end)
+					Variables.Stepped = Connect(Service"Run".Stepped,function()
+						if not Variables.FoundCoin then
+							return
+						end
+						for _,BasePart in Valid.Table(Character and Character:GetChildren()) do
+							if Valid.Instance(BasePart,"BasePart") then
+								BasePart.AssemblyLinearVelocity,BasePart.CanCollide,BasePart.CanTouch,BasePart.CanQuery = Vector3.zero,BasePart.Name == "HumanoidRootPart" and Variables.Collecting,Variables.Collecting,Variables.Collecting
+							end
+						end
+					end)
+					Variables.Connection = Connect(Service"Run".Heartbeat,function()
+						Variables.Delta,Variables.LastFrame = math.min(os.clock()-Variables.LastFrame,1/15)*60,os.clock()
+						for Coin,Time in Variables.IgnoreCoins do
+							if not Coin:IsDescendantOf(workspace) or 3 < Variables.LastFrame-Time then
+								Variables.IgnoreCoins[Coin] = nil
+							end
+						end
+						Variables.FoundCoin = false
+						if not Variables.Collecting then
+							local Distance,Coin = math.huge,nil
+							for _,NewCoin in Variables.Coins do
+								local Magnitude = (NewCoin.Position-Variables.Position).Magnitude
+								if not Variables.IgnoreCoins[NewCoin] and not NewCoin:FindFirstChildWhichIsA"Model" and NewCoin:FindFirstChild"Coin" and Magnitude < Distance then
+									Coin,Distance = NewCoin,Magnitude
+								end
+							end
+							if Coin and Distance < 250 and Variables.Position ~= Coin.Position then
+								Variables.FoundCoin = true
+								Variables.Position = CFrame.lookAt(Variables.Position,Coin.Position)*CFrame.new(0,0,-math.min(25*Variables.Delta/60,Distance)).Position
+								Character:PivotTo(CFrame.new(Variables.Position-Vector3.yAxis*2)*CFrame.Angles(math.pi/2,0,0))
+								if (Variables.Position-Coin.Position).Magnitude < .01 then
+									Variables.Collecting = true
+									Variables.IgnoreCoins[Coin] = os.clock()
+									repeat
+										Wait()
+										Character:PivotTo(CFrame.new(Variables.Position)*CFrame.Angles(math.pi/2,0,0))
+									until .25 < os.clock()-Variables.IgnoreCoins[Coin]
+									Variables.Collecting = false
+								end
+							else
+								Variables.Position = Character:GetPivot().Position
+							end
+						end
+					end)
+					AddConnections{
+						Variables.CharacterAdded,
+						Variables.Connection,
+						Variables.CoinAdded,
+						Variables.Stepped
+					}
+					Variables.Enabled = true
+				else
+					RemoveConnections{
+						Variables.CharacterAdded,
+						Variables.Connection,
+						Variables.CoinAdded,
+						Variables.Stepped
+					}
+					Variables.Enabled = false
+				end
+			end,
+			Arguments = {
+				{
+					Name = "Enabled",
+					Type = "Boolean",
+					Substitute = true
+				}
+			},
+			Variables = {}
 		}
 	},
 	_6755746130 = {
