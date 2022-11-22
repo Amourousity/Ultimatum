1fa3abd0e8e925026546dde17163958f0ee999a4
Lex <87553279+Amourousity@users.noreply.github.com>
2022-11-22 09:55:10 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,16 +8,15 @@
 local function Set(Source)
 	loadstring(Source or readfile"Ultimatum.lua","Ultimatum")()
 end
+local Success,Result = pcall(game.HttpGet,game,"https://raw.githubusercontent.com/Amourousity/Ultimatum/main/Source.lua",true)
+if Success then
+	if isfile then
+		writefile("Ultimatum.lua",Result)
+	else
+		Set(Result)
+		return
+	end
+end
 if isfile and isfile"Ultimatum.lua" then
 	Set()
-else
-	local Success,Result = pcall(game.HttpGet,game,"https://raw.githubusercontent.com/Amourousity/Ultimatum/main/Source.lua",true)
-	if Success then
-		if isfile then
-			writefile("Ultimatum.lua",Result)
-			Set()
-		else
-			Set(Result)
-		end
-	end
 end
