78460f80649c3275f999d4054870dc5cdcae42ce
Lex <87553279+Amourousity@users.noreply.github.com>
2022-11-24 01:24:55 -0500

    Update 537413528.lua

--- CommandSets/537413528.lua
+++ CommandSets/537413528.lua
@@ -36,7 +36,7 @@ return {
 						if Character ~= Variables.CurrentCharacter then
 							Wait(2)
 							Variables.CurrentStep = 1
-							Variables.CameraPosition = workspace.Currentcamera.CFrame
+							Variables.CameraPosition = workspace.CurrentCamera.CFrame
 							Variables.Position = workspace.CurrentCamera.Focus.Position-Vector3.yAxis*10
 							Variables.CurrentCharacter = Character
 						end
